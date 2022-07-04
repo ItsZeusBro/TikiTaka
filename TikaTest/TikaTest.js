e977fb378aeb15a76cd04c72acf486838a016efc
Zeus <zeusofolympus1000@gmail.com>
2022-07-04 14:35:42 -0400

    magical stuff

--- TikaTest/TikaTest.js
+++ TikaTest/TikaTest.js
@@ -3,26 +3,12 @@ import { TikaPrims } from "../TikaPrims/TikaPrims.js"
 export class TikaTest{
     constructor(logFile, verbose, comnt){
         this.tp = new TikaPrims()
-        this.verbose=verbose
-        this.comnt=comnt
-        this.logFile=logFile
         process.on('uncaughtException', err => {
-            this.comment('There was an uncaught error\n'+err);
+            this.comnt('There was an uncaught error\n'+err);
             process.exit(1); // mandatory (as per the Node.js docs)
         });
     }
-    comment(...data){
-        var obj={}
-
-        obj[this.logFile]=data.join(' ')+'\n'
-
-        if (this.logit){
-            this.tp.write(obj)
-        }
-        if (this.verbose){
-            console.log(data)
-        }
-    }
+    
     clean(dir){
         this.tp.del(true, dir)
     }
--- TikaTests.js
+++ TikaTests.js
@@ -4,34 +4,48 @@ import { TikaInterfaceTest } from "./TikaTest/TikaInterface/TikaInterfaceTest.js
 import process from 'node:process';
 import { TikaPrims } from "./TikaPrims/TikaPrims.js";
 
-class TikaTests{
+class Comnt{
     constructor(){
         this.verbose;
-        this.log;
         this.flags()
         this.instance = Date.now()
         this.logDir = "./logs/"+this.instance+'/'
         this.tp = new TikaPrims()
         this.tp.mkdr(this.logDir)
-        
-        this.tt = new TikaTest(this.logDir+"TikaTest.log", this.verbose, this.log) 
-        this.ttpt = new TikaPrimsTest(this.logDir+"TikaPrimsTest.log", this.verbose, this.log)
-        this.tit = new TikaInterfaceTest(this.logDir+"TikaInterfaceTest.log", this.verbose, this.log)
     }
+
     flags(){
         var flags = process.argv
         flags.forEach(element => {
             if (element=='--verbose'){
                 this.verbose=true
             }
-            if (element=='--log'){
-                this.log=true
-            }
-
         });
+    }
+    comnt(...data){
+        //Keep all this magical stuff right here, don't separate it
+        var stk = new Error().stack
+        var clss = Array(stk.split('at')[1].split('.'))[0][0].trim()
+        console.log(clss)
+        var obj={}
+        obj['./logs/'+clss+'.log']=data+'\n'
+        this.tp.write(obj)
+
+        if (this.verbose){
+            console.log(data.join(' '))
+        }
+
+
     }
 
 }
 
-
-var tikaTests = new TikaTests()
+class TikaTests extends Comnt{
+    constructor(){
+        super()
+        // this.tt = new TikaTest() 
+        // this.ttpt = new TikaPrimsTest()
+        // this.tit = new TikaInterfaceTest()
+        this.comnt()
+    }
+}
